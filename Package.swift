53322742174d2303f1c1b221a2ce3b23975223c7
Juan Carlos <63151705+j-aguilar-garcia@users.noreply.github.com>
2025-12-12 13:08:12 +0100

    Bump version number

--- Package.swift
+++ Package.swift
@@ -19,7 +19,7 @@ let package = Package(
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .binaryTarget(name: "ChilkatPackage",
-                      url: "https://github.com/K3V-Solutions/ChilkatPackage/releases/download/1.0.0/ChilkatPackage.xcframework.zip",
+                      url: "https://github.com/K3V-Solutions/ChilkatPackage/releases/download/1.0.1/ChilkatPackage.xcframework.zip",
                       checksum: "ea9e832d678f7334428105e9c91459c34a148a49d49ef355910c80025429f40e")
     ]
 )
